78aed642bd4a873ab7e8b6c82f5972908926a04b
monica <monica@monicacentos>
2020-04-11 08:42:13 -0600

    Modified to be able to run installation from requirements text file

--- RestDemo/RestDemo/settings.py
+++ RestDemo/RestDemo/settings.py
@@ -14,19 +14,6 @@ import os
 #import RestDemo
 
 
-from django.core.exceptions import ImproperlyConfigured
-"""
-def get_env_variable(var_name):
-    try:
-        return os.environ[var_name]
-    except KeyError:
-        error_msg = "Set the %s environment variable    " % var_name
-        raise ImproperlyConfigured(error_msg)
-
-SECRET_KEY = get_env_variable('SECRET_KEY')
-"""
-
-
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
 
