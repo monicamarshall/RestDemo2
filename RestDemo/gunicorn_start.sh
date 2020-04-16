52b5be05420c961739c82cdb2c3fa284eaa491cd
monica <monica@monicacentos>
2020-04-16 08:01:11 -0600

    Updated script to start gunicorn for RestDemo2

--- RestDemo/gunicorn_start.sh
+++ RestDemo/gunicorn_start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 NAME="RestDemo"                              #Name of the application (*)
-DJANGODIR=/home/monicarhvm/pythonprojects/RestDemo             # Django project directory (*)
-SOCKFILE=/home/monicarhvm/pythonprojects/envs/restdemo/bin/gunicorn.sock        # we will communicate using this unix socket (*)
-USER=monicarhvm                                        # the user to run as (*)
-GROUP=monicarhvm                                     # the group to run as (*)
+DJANGODIR=/home/monica/pythonworkspace/RestDemo2/RestDemo             # Django project directory (*)
+SOCKFILE=/home/monica/envs/restdemo/bin/gunicorn.sock        # we will communicate using this unix socket (*)
+USER=monica                                        # the user to run as (*)
+GROUP=monica                                    # the group to run as (*)
 NUM_WORKERS=1                                     # how many worker processes should Gunicorn spawn (*)
 DJANGO_SETTINGS_MODULE=RestDemo.settings             # which settings file should Django use (*)
 DJANGO_WSGI_MODULE=RestDemo.wsgi                     # WSGI module name (*)
@@ -13,7 +13,7 @@ echo "Starting $NAME as `whoami`"
 
 # Activate the virtual environment
 cd $DJANGODIR
-source /home/monicarhvm/pythonprojects/envs/restdemo/bin/activate
+source /home/monica/envs/restdemo/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
@@ -23,7 +23,7 @@ test -d $RUNDIR || mkdir -p $RUNDIR
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-exec /home/monicarhvm/pythonprojects/envs/restdemo/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+exec /home/monica/envs/restdemo/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
   --name $NAME \
   --workers $NUM_WORKERS \
   --user $USER \
